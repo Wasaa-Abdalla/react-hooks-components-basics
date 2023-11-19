1ec5a43ffc36086d077e85f49d789a4046e10369
abdalla-wasaa <abdallawasaa7@gmail.com>
2023-11-19 09:35:59 +0300

    Added Trial Conponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Trial from "./Trial";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Trial/>
     </div>
   );
 }
---
+++ src/Trial.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Trial() {
+  return <div>My first React Component</div>;
+}
+
+export default Trial;
